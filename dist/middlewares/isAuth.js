c1bc409197e373fe4ca4a342b5e09a4ec409ee6b
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-06-11 17:56:10 +0545

    .gitignore updated

--- dist/middlewares/isAuth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const isAuth = (req, res, next) => {
-    if (!req.session.isLoggedIn) {
-        return res.redirect('/auth/login');
-    }
-    next();
-};
-exports.default = isAuth;
--- dist/middlewares/notAuth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notAuth = (req, res, next) => {
-    if (req.session.isLoggedIn) {
-        return res.redirect('/');
-    }
-    next();
-};
-exports.default = notAuth;
